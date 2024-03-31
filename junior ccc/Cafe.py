7fe700d5033fab44609e0685b1cc9a48d7cca778
shree <163702108+90shree@users.noreply.github.com>
2024-03-31 01:25:19 -0400

    Update Cafe.py

--- junior ccc/Cafe.py
+++ junior ccc/Cafe.py
@@ -1,11 +1,22 @@
-menu = {"chocolate cake","vanilla cake","strawberry cake"}
-price = {"2.00","3.00","4.00"}
+menu = {"chocolate cake","vanilla cake","strawberry cake","coffee","latte","capuccino"}
+price = {"2.00","3.00","4.00","1.00","2.00","5.00"}
 yes_list = {"yes","Yes","y"}
 
-while (True):
-    order = input("What would you like to order? ")
 
-    if (order in menu):
+print(menu,price)
+while (True):
+    order = input("Would you like food or a drink? ")
+
+
+    if (order == "food"):
+        food = ("Enter the food item you'd like to order: ")
+        final_order = food
+
+    elif (order == "drink"):
+        drink = ("Enter the drink you'd like to order: ")
+        final_order = food
+        
+    if (final_order in menu):
         print("You have ordered a",order)
         clarification = input("is that correct?")
 
